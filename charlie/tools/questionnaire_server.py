d5d1aac5873d1d73449ad62703499c0ad03a228a
smathias <samuel.mathias@gmail.com>
2015-03-04 16:31:20 -0500

    adding web server feature for questionnaires

---
+++ charlie/tools/questionnaire_server.py
@@ -0,0 +1,17 @@
+__author__ = 'smathias'
+
+import web
+
+urls = (
+  '/', 'index'
+)
+
+app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        greeting = "Hello World"
+        return greeting
+
+if __name__ == "__main__":
+    app.run()
